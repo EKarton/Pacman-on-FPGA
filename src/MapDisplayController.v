44e9567cf687c23e34a68f315b7acdb2087503ea
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2018-03-20 00:03:05 -0400

    Added test data for MapDisplayController

--- src/MapDisplayController.v
+++ src/MapDisplayController.v
@@ -1,62 +1,144 @@
 /*
 	A controller used to display the map to the screen
+	To display data to the screen, set en = 1, reset first to 1, then to 0, 
+	attach map_x, map_y, sprite_type to the MapController (to get data),
+	attach the vga_plot, vga_x, vga_y, and vga_color to the VGA adapter.
  */
 module MapDisplayController(
-	input plot, 
-	output reg [4:0] x_out, 
-	output reg [4:0] y_out, 
-	input [3:0] type, 
-	input en,
+	input en, 
+	output reg [4:0] map_x, 
+	output reg [4:0] map_y, 
+	input [3:0] sprite_type, 
 	output reg vga_plot, 
-	output reg [7:0] vga_x,
-	output reg [6:0] vga_y,
+	output [7:0] vga_x,
+	output [6:0] vga_y,
 	output reg [2:0] vga_color,
 	input reset, 
 	input clock_50);
 
-	// Getting the x and y coordinates of the blocks (note that the grid is 21 x 21!)
+	// Going through the grid (note that the grid is 21 x 21!)
+	reg [4:0] cur_sprite_x;
+	reg [4:0] cur_sprite_y;
+
 	always @(posedge clock_50) 
 	begin
-		if (reset == 1'b1 || y_out == 5'd21) 
+		if (reset == 1'b1 || map_y == 5'd21) 
 		begin
-			x_out <= 5'd0;
-			y_out <= 5'd0;	
-			vga_plot <= 1'b0;
+			map_x <= 5'd0;
+			map_y <= 5'd0;
+			cur_sprite_x <= 5'd0;
+			cur_sprite_y <= 5'd0;	
+			vga_plot <= 1'b1;
 		end
-		else 
+		else if (en == 1'b1)
 		begin
-			if (x_out == 5'd21 && y_out < 5'd21)
+			// If we are currently drawing the sprite
+			if (cur_sprite_x < 5'd7 || cur_sprite_y < 5'd7)
 			begin
-				x_out <= 5'd0;
-				y_out <= y_out + 5'd1;
-				vga_plot <= 1'b1;
+
+				// If we have finished drawing one row, go to next row
+				if (cur_sprite_x == 5'd7)
+				begin
+					cur_sprite_x <= 5'd0;
+					cur_sprite_y <= cur_sprite_y + 5'd1;									
+				end				
+
+				// if we are not finished drawing a row, continue on the row
+				else 
+				begin
+					cur_sprite_x <= cur_sprite_x + 5'd1;					
+				end
 			end
+
+			// If we have finished drawing the sprite
 			else 
 			begin
-				x_out <= x_out + 5'd1;				
-				vga_plot <= 1'b1;
-			end			
+				// Reset the current sprite coordinates
+				cur_sprite_x <= 5'd0;
+				cur_sprite_y <= 5'd0;			
+
+				if (map_x == 5'd21 && map_y < 5'd21)
+				begin
+					map_x <= 5'd0;
+					map_y <= map_y + 5'd1;
+				end
+				else 
+				begin
+					map_x <= map_x + 5'd1;				
+				end	
+			end		
 		end
 	end	
-	
-	// Handle the colors
-	always @(posedge clock_50)
+
+	// Determine the absolute pixel coordinates on the screen
+	assign vga_x = (map_x * 5'd7) + cur_sprite_x + 5'd1;
+	assign vga_y = (map_y * 5'd7) + cur_sprite_y + 5'd1;
+
+	// Determining the sprite
+	reg [6:0] row0;
+	reg [6:0] row1;
+	reg [6:0] row2;
+	reg [6:0] row3;
+	reg [6:0] row4;
+	reg [6:0] row5;
+	reg [6:0] row6;
+
+	always @(*)
 	begin
-		if (reset == 1'b1)
+		if (sprite_type == 3'b000) // A black tile
 		begin
-			vga_color <= 3'b00;
+			row0 = 7'b0000000;
+			row1 = 7'b0000000;
+			row2 = 7'b0000000;
+			row3 = 7'b0000000;
+			row4 = 7'b0000000;
+			row5 = 7'b0000000;
+			row6 = 7'b0000000;
+			vga_color = 3'b000;
 		end
-		else
+		else if (sprite_type == 3'b001) // A big orb
 		begin
-			vga_x <= x_out;
-			vga_y <= y_out;
-			case (type)
-				3'd0: vga_color <= 3'b010; // Black tile
-				3'd3: vga_color <= 3'b001; // Blue tile
-				3'd2: vga_color <= 3'b111; // Small orb tile
-				3'd1: vga_color <= 3'b111; // Big orb tile
-				3'd4: vga_color <= 3'b100; // Grey tile
-			endcase
+			row0 = 7'b0000000;
+			row1 = 7'b0011100;
+			row2 = 7'b0111110;
+			row3 = 7'b0111110;
+			row4 = 7'b0111110;
+			row5 = 7'b0011100;
+			row6 = 7'b0000000;
+			vga_color = 3'b111;
+		end
+		else if (sprite_type == 3'b010) // A small orb
+		begin
+			row0 = 7'b0000000;
+			row1 = 7'b0000000;
+			row2 = 7'b0011100;
+			row3 = 7'b0011100;
+			row4 = 7'b0011100;
+			row5 = 7'b0000000;
+			row6 = 7'b0000000;
+			vga_color = 3'b111;
+		end
+		else if (sprite_type == 3'b011) // A blue tile
+		begin
+			row0 = 7'b1111111;
+			row1 = 7'b1111111;
+			row2 = 7'b1111111;
+			row3 = 7'b1111111;
+			row4 = 7'b1111111;
+			row5 = 7'b1111111;
+			row6 = 7'b1111111;
+			vga_color = 3'b001;
+		end
+		else if (sprite_type == 3'b100) // A grey tile
+		begin
+			row0 = 7'b1111111;
+			row1 = 7'b1111111;
+			row2 = 7'b1111111;
+			row3 = 7'b1111111;
+			row4 = 7'b1111111;
+			row5 = 7'b1111111;
+			row6 = 7'b1111111;
+			vga_color = 3'b010;
 		end
 	end
 endmodule
---
+++ tests/MapDisplayController_Tests.do
@@ -0,0 +1,24 @@
+vlib work
+vlog -timescale 1ns/1ns "../src/MapDisplayController.v"
+vsim MapDisplayController
+log {/*}
+add wave {/*}
+
+#module MapDisplayController(
+#	output reg [4:0] x_out, 
+#	output reg [4:0] y_out, 
+#	input [3:0] type, 
+#	input en,
+#	output reg vga_plot, 
+#	output reg [7:0] vga_x,
+#	output reg [6:0] vga_y,
+#	output reg [2:0] vga_color,
+#	input reset, 
+#	input clock_50);
+
+force {reset} 1 0ns, 0 10ns
+force {clock_50} 0 0ns, 1 5ns -r 10ns
+force {type} 3'b000
+force {en} 1
+
+run 3000ns
